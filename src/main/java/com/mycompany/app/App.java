0dc2afdafb956820affd2f879a224d2543295e69
Amol Chitte <amol.chitte@tatatechnologies.com>
2020-09-11 22:49:18 +0530

    OK

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,15 +3,16 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App
-{
+public class App {
 
     private final String message = "Hello World!";
 
-    public App() {}
+    public App() {
+    }
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println("TEST ME");
     }
 
     private final String getMessage() {
